ec8e29f011126c47b8b64f74c51742bb424080b8
Jennifer Vendetti <vendetti@stanford.edu>
2013-12-12 16:19:54 -0800

    initial commit of project files

---
+++ src/main/java/org/protege/integration/hermit/ProtegeReasonerFactory.java
@@ -0,0 +1,35 @@
+package edu.stanford.protege.integration.hermit;
+
+import org.protege.editor.owl.model.inference.AbstractProtegeOWLReasonerInfo;
+import org.semanticweb.HermiT.Configuration;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.reasoner.BufferingMode;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.ReasonerProgressMonitor;
+
+public class ProtegeReasonerFactory extends AbstractProtegeOWLReasonerInfo {
+
+	protected final Reasoner.ReasonerFactory factory = new Reasoner.ReasonerFactory();
+
+	@Override
+	public OWLReasonerConfiguration getConfiguration(ReasonerProgressMonitor monitor) {
+		Configuration configuration = new Configuration();
+		configuration.ignoreUnsupportedDatatypes = true;
+		configuration.reasonerProgressMonitor = monitor;
+		return configuration;
+	}
+
+	@Override
+	public BufferingMode getRecommendedBuffering() {
+		return BufferingMode.BUFFERING;
+	}
+
+	@Override
+	public OWLReasonerFactory getReasonerFactory() {
+		return factory;
+	}
+}
+
+
+
